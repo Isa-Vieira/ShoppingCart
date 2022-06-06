01925d1a2c0076baac1222f152818607db2b11ec
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-06-06 18:35:27 -0300

    requisito-1-concluido

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,8 @@
-const fetchProducts = () => {
-  // seu código aqui
-};
+const fetchProducts = (nomeProduto) => 
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${nomeProduto}`)
+  .then((item1) => item1.json())
+  .then((item2) => item2)
+  .catch((error) => error);
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -11,21 +11,19 @@ describe('1 - Teste a função fetchProducts', () => {
 });
 test('Se fetch foi chamada', async () => {
   const resposta1 = await fetchProducts('computador')
-  expect(resposta1).toHaveBeenCalled('fetch')
+  expect(fetch).toHaveBeenCalled()
 });
 test('Se a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async() => {
-  const resposta2 = await fetchProducts('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-  expect(resposta2).toBe('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+   await fetchProducts('computador')
+  expect(fetch).toHaveBeenCalledWith(url)
 });
 test('Se o retorno da função é uma estrutura de dados igual ao objeto computadorSearch' , async () => {
-  const resposta3 = await fetchProducts('computadorSearch')
-  expect(resposta3).toEqual('computadorSearch')
+  const resposta3 = await fetchProducts('computador')
+  expect(resposta3).toEqual(computadorSearch)
 });
 test('Se ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url"', async () => {
-  try {
-    expect.assertions(1)
-    const resposta = await fetchProducts('You must provide an url');
-  } catch (error) {
-    expect(error).toEqual(new Error('You must provide an url'));
+    const resposta = await fetchProducts()
+    expect(resposta).toEqual(new Error('You must provide an url'));
   }
-});
+);
