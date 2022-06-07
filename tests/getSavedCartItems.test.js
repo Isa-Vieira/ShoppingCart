69fe6d828166e358f67f6807e50bdb0130bf443c
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-06-07 17:15:36 -0300

    requisito-7-concluido

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,14 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test(' Se ao executar getSavedCartItems o o método localStorage.setItem é chamado',  () => {
+    getSavedCartItems()
+    expect(localStorage.setItem).toHaveBeenCalled();
+  })
+  /* fail('Teste vazio'); */
+
+  test(' Se ao executar getSavedCartItems o o método localStorage.setItem é chamado com um parametro ', () => {
+    const test = getSavedCartItems()
+    expect(localStorage.setItem).toHaveBeenCalled(localStorage.setItem('cartItems'))
+  });
 });
